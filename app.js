f9cd39ee6367b63554159fd703bb8bac5079e7ee
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-09-30 14:31:54 -0700

    Working add feature

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { add } from './calculations.js';
+
 const addInput1 = document.getElementById('add-input1'); 
 const addInput2 = document.getElementById('add-input2');
 const addButton = document.getElementById('add-btn');
@@ -6,5 +8,8 @@ const addAnswer = document.getElementById('add-answer');
 console.log (addInput1, addInput2, addButton, addAnswer);
 
 addButton.addEventListener('click', () => {
-  console.log("add button is being clicked")
+  const value1 = Number(addInput1.value);
+  const value2 = Number(addInput2.value);
+  const result = add(value1, value2);
+  addAnswer.textContent = result;
 }); 
